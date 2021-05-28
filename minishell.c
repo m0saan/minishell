72b12bb4ac01f37b4b90a303cbf9f106ae636cb4
mo-san99 <moboustta6@gmail.com>
2021-05-28 18:30:26 +0100

    split the entered command by semicolon

--- minishell.c
+++ minishell.c
@@ -50,7 +50,7 @@ const char *getTypeName(enum e_val_type type) {
     }
 }
 
-char * Print(struct s_cmd *this) {
+char *Print(struct s_cmd *this) {
 
     if (this == NULL)
         ft_exit("Error\nNull CMD!", 1);
@@ -60,7 +60,7 @@ char * Print(struct s_cmd *this) {
     bzero(line, 1024);
     for (int i = 0; this->argv[i] != 0; i++) {
         strcat(line, this->argv[i]);
-        if (this->argv[i+1] != 0)
+        if (this->argv[i + 1] != 0)
             strcat(line, " ");
     }
     return line;
@@ -76,50 +76,43 @@ t_cmd *create_cmd() {
 t_vector *fill_out_vector_with_commands(t_node *ast_node) {
     t_node *child;
     t_vector *vector = new_vector();
-    t_vector *tmp_vector = NULL;
     t_cmd *tmp_cmd = NULL;
 
 
-    while (ast_node) {
-        child = ast_node->first_child;
-        tmp_vector = new_vector();
-        tmp_cmd = create_cmd();
-        while (child) {
-            if (child->val_type == right) {
-                struct s_redir *tmp = create_redir(right, child->next_sibling->val.str);
-                child = child->next_sibling;
-                insert(tmp_cmd->redirs, tmp);
-            } else if (child->val_type == left) {
-                insert(tmp_cmd->redirs, create_redir(left, child->next_sibling->val.str));
-                child = child->next_sibling;
-            } else if (child->val_type == right_append) {
-                insert(tmp_cmd->redirs, create_redir(right_append, child->next_sibling->val.str));
-                child = child->next_sibling;
-            } else if (child->val.str[0] == '$' && child->val_type == env_var) {
-                if (child->val.str != NULL) {
-                    char *tmp =  handle_env_variables(child->val.str);
-                    if (tmp != NULL)
-                        tmp_cmd->argv[tmp_cmd->count++] = tmp;
-                }
-                else
-                    tmp_cmd->argv[tmp_cmd->count++] = child->val.str;
-            } else if (child->val_type == _pipe) {
-                insert(tmp_vector, tmp_cmd);
-                tmp_cmd = create_cmd();
+    child = ast_node->first_child;
+    tmp_cmd = create_cmd();
+    while (child) {
+        if (child->val_type == right) {
+            struct s_redir *tmp = create_redir(right, child->next_sibling->val.str);
+            child = child->next_sibling;
+            insert(tmp_cmd->redirs, tmp);
+        } else if (child->val_type == left) {
+            insert(tmp_cmd->redirs, create_redir(left, child->next_sibling->val.str));
+            child = child->next_sibling;
+        } else if (child->val_type == right_append) {
+            insert(tmp_cmd->redirs, create_redir(right_append, child->next_sibling->val.str));
+            child = child->next_sibling;
+        } else if (child->val.str[0] == '$' && child->val_type == env_var) {
+            if (child->val.str != NULL) {
+                char *tmp = handle_env_variables(child->val.str);
+                if (tmp != NULL)
+                    tmp_cmd->argv[tmp_cmd->count++] = tmp;
             } else
                 tmp_cmd->argv[tmp_cmd->count++] = child->val.str;
-            child = child->next_sibling;
-        }
-        insert(tmp_vector, tmp_cmd);
-        insert(vector, tmp_vector);
-        ast_node = ast_node->next_sibling;
+        } else if (child->val_type == _pipe) {
+            insert(vector, tmp_cmd);
+            tmp_cmd = create_cmd();
+        } else
+            tmp_cmd->argv[tmp_cmd->count++] = child->val.str;
+        child = child->next_sibling;
     }
+    insert(vector, tmp_cmd);
     return vector;
 }
 
 t_error *check_first_token(t_parser *p) {
     t_error *error = malloc(sizeof(t_error));
-    ft_memset(error, 0, sizeof (t_error));
+    ft_memset(error, 0, sizeof(t_error));
     enum e_val_type types[] = {illegal, end_of, semicolon, _pipe, right, left, right_append};
     for (int i = 0; i < 7; ++i) {
         if ((p->cur_token->Type == types[i] && p->peek_token->Type == end_of) || p->peek_token->Type == end_of)
@@ -147,19 +140,13 @@ void parse_and_execute(t_lexer *lexer) {
     if (ast_node == NULL)
         return;
     t_vector *vector = fill_out_vector_with_commands(ast_node);
-    int i = 0;
-    while (i < vector->size)
-    {
-        run_cmds((t_vector *)at(vector, i));
-        ++i;
-    }
+    run_cmds((t_vector *) vector);
 }
 
 #if (1)
 
-int main(int ac, char **av, char **env)
-{
-	fill_envp(env);
+int main(int ac, char **av, char **env) {
+    fill_envp(env);
     while (true) {
         write(1, prompt, ft_strlen(prompt));
         char *line = malloc(1024 * sizeof(char));
@@ -170,16 +157,19 @@ int main(int ac, char **av, char **env)
         }
 
         if (strcmp(line, "exit") == 0) {
-            printf("shell is exiting...\n");
+            dprintf(2, "shell is exiting...\n");
             free(line);
             break;
         }
 
         char **splited_line = ft_split(line, ';');
-        int  i = 0;
-
-        t_lexer *lexer = new_lexer(line, (int) n);
-        parse_and_execute(lexer);
+        int i = 0;
+        while (splited_line[i] != 0) {
+            t_lexer *lexer = new_lexer(splited_line[i], (int) ft_strlen(splited_line[i]));
+            parse_and_execute(lexer);
+            i++;
+            free(lexer);
+        }
         if (errno == ENODATA)
             ft_exit("TTTT", errno);
 
