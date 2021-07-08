9ad2cd5fe07d15fc01532008a022e9fa383c83b1
MoBoustta <moboustta6@gmail.com>
2021-07-08 13:32:57 +0100

    handle ~ cases

--- tokenizer/token.c
+++ tokenizer/token.c
@@ -9,7 +9,7 @@ t_token_type	lookup_type(char *ident)
 	i = -1;
 	while (++i < 7)
 	{
-		if (strcmp(ident, commands[i]) == 0)
+		if (ft_strcmp(ident, commands[i]) == 0)
 			return (COMMAND);
 	}
 	return (ARG);
@@ -19,6 +19,12 @@ t_token *new_token(t_token_type token_type, char ch, t_token *token)
 {
 	token->type = token_type;
 	token->letter_literal = ch;
-	token->literal = NULL;
+	if (token_type == TILDE)
+	{
+		next_char(token->lexer);
+		token->literal = read_identifier(token->lexer, 0);
+	}
+	else
+		token->literal = NULL;
 	return (token);
 }
