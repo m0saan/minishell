9ae0e59dfd6ad080dd1845cf0646d21ecc8027b6
Elkebir Hakam <ehakam@e0r3p11.1337.ma>
2021-07-14 21:52:02 +0100

    FUUUUUUCK

--- global_utils/ft_atol.c
+++ global_utils/ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: ehakam <ehakam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/04 19:20:09 by ehakam            #+#    #+#             */
-/*   Updated: 2021/07/04 19:22:21 by ehakam           ###   ########.fr       */
+/*   Updated: 2021/07/14 20:26:34 by ehakam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ long long	ft_atol_err(char *str)
 	int			i;
 	int			n;
 	long long	r;
-	const char	*err_msg = "numeric argument required";
 
 	i = 0;
 	r = 0;
@@ -30,14 +29,8 @@ long long	ft_atol_err(char *str)
 	else if (str[i] == '+')
 		++i;
 	while (str[i] >= '0' && str[i] <= '9')
-	{
 		r = r * 10 + (str[i++] - '0');
-		if ((n == 1 && r > MAXVAL) || (n == -1 && r > MINVAL))
-			exit(p_error("exit", str, err_msg, (unsigned char)(r * n)));
-	}
 	while (str[i] > 0 && str[i] < 33 && str[i] != 27)
 		i++;
-	if (str[i] != '\0')
-		exit(p_error("exit", str, err_msg, 255));
 	return (r * n);
 }
